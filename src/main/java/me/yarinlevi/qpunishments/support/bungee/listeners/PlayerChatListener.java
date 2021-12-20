7652e231516df630e6f2fe65e296f34459a51bc3
YarinQuapi <41652571+YarinQuapi@users.noreply.github.com>
2021-12-20 14:18:59 +0200

    Moved mute handling to caching to improve performance

--- src/main/java/me/yarinlevi/qpunishments/support/bungee/listeners/PlayerChatListener.java
+++ src/main/java/me/yarinlevi/qpunishments/support/bungee/listeners/PlayerChatListener.java
@@ -8,50 +8,71 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class PlayerChatListener implements Listener {
     private final boolean staffChatEnabled = QBungeePunishments.getInstance().getConfig().getBoolean("staff-chat.enabled");
     @Setter private boolean isQProxyUtilitiesFound = false;
 
-    @EventHandler
-    public void onPlayerChat(ChatEvent event) throws SQLException {
+    private final Map<UUID, PlayerCache> cache = new HashMap<>();
+
+    public PlayerChatListener() {
+        QBungeePunishments.getInstance().getProxy().getScheduler().schedule(QBungeePunishments.getInstance(), cache::clear, 10L, TimeUnit.MINUTES);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerChat(ChatEvent event) {
         if (event.getSender() instanceof ProxiedPlayer sender) {
-            String sql = String.format("SELECT * FROM punishments WHERE `punished_uuid`=\"%s\" AND `punishment_type`=\"mute\" AND `expire_date` > \"%s\" OR `punished_uuid`=\"%s\" AND `expire_date`=0 AND `punishment_type`=\"mute\" ORDER BY id DESC;",
-                    sender.getUniqueId().toString(), System.currentTimeMillis(), sender.getUniqueId().toString());
+            if (cache.containsKey(sender.getUniqueId())) {
 
-            ResultSet rs = QBungeePunishments.getInstance().getMysql().get(sql);
+                event.setCancelled(this.process(sender, event.getMessage()));
 
-            if (rs != null && rs.next() && !rs.getBoolean("bypass_expire_date")) {
-                String server = rs.getString("server");
+            } else {
+                String sql = String.format("SELECT * FROM punishments WHERE `punished_uuid`=\"%s\" AND `punishment_type`=\"mute\" AND `expire_date` > \"%s\" OR `punished_uuid`=\"%s\" AND `expire_date`=0 AND `punishment_type`=\"mute\" ORDER BY id DESC;",
+                        sender.getUniqueId().toString(), System.currentTimeMillis(), sender.getUniqueId().toString());
 
-                if (server.equalsIgnoreCase("global") || sender.getServer().getInfo().getName().equals(server)) {
-                    event.setCancelled(true);
+                ResultSet rs = QBungeePunishments.getInstance().getMysql().get(sql);
 
-                    long timestamp = rs.getLong("expire_date");
+                try {
+                    if (rs != null && rs.next()) {
+                        String server = rs.getString("server");
+                        long timestamp = rs.getLong("expire_date");
+                        boolean bypass = rs.getBoolean("bypass_expire_date");
 
-                    String formattedDate;
-                    if (timestamp != 0) {
-                        formattedDate = new SimpleDateFormat(MessagesUtils.getRawString("date_format")).format(new Date(timestamp));
+                        PlayerCache playerCache = new PlayerCache(sender.getUniqueId(), true, timestamp, bypass, server);
+
+                        cache.put(playerCache.uuid, playerCache);
+
+                        event.setCancelled(this.process(sender, event.getMessage()));
+
                     } else {
-                        formattedDate = "forever";
-                    }
+                        PlayerCache playerCache = new PlayerCache(sender.getUniqueId(), false, 0, false, "dead");
 
-                    sender.sendMessage(MessagesUtils.getMessage("you_are_muted", formattedDate));
+                        cache.put(playerCache.uuid, playerCache);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    QBungeePunishments.getInstance().getLogger().severe("Hey! an SQL error was detected! please check your configuration files");
                 }
             }
+
 
             if (sender.hasPermission("qpunishments.commands.staffchat") && staffChatEnabled) {
                 if (event.getMessage().startsWith(QBungeePunishments.getInstance().getConfig().getString("staff-chat.chat-char"))) {
                     if (!isQProxyUtilitiesFound) {
                         event.setCancelled(true);
 
-                        StringBuffer sb = new StringBuffer(event.getMessage());
+                        StringBuilder sb = new StringBuilder(event.getMessage());
 
                         sb.deleteCharAt(0);
 
@@ -65,6 +86,40 @@ public class PlayerChatListener implements Listener {
                     }
                 }
             }
+        }
+    }
+
+    private boolean process(ProxiedPlayer player, String args) {
+        PlayerCache playerCache = cache.get(player.getUniqueId());
+
+        if (playerCache.isPunished(player.getServer().getInfo().getName())) {
+            if (args.charAt(0) == '/') {
+                String command = new StringBuilder(args).deleteCharAt(0).toString();
+
+                for (String cmd : QBungeePunishments.getInstance().getConfig().getStringList("blocked-commands")) {
+                    if (command.toLowerCase().startsWith(cmd.toLowerCase())) {
+                        return true; // Cancels
+                    }
+                }
+
+                return false; // Allows
+            } else {
+                String formattedDate;
+                if (playerCache.until != 0) {
+                    formattedDate = new SimpleDateFormat(MessagesUtils.getRawString("date_format")).format(new Date(playerCache.until));
+                } else {
+                    formattedDate = "forever";
+                }
+
+                player.sendMessage(MessagesUtils.getMessage("you_are_muted", formattedDate));
+                return true; // Cancels
+            }
+        } else return false; // Allows
+    }
+
+    private record PlayerCache(UUID uuid, boolean valid, long until, boolean bypass, String server) {
+        public boolean isPunished(String server) {
+            return (!valid || bypass || System.currentTimeMillis() > until) && (this.server.equalsIgnoreCase("global") || server.equalsIgnoreCase(this.server));
         }
     }
 }
