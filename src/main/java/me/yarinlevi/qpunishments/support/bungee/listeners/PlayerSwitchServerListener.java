367cd45bae6d40911907c2c9692bb4e151087d05
YarinQuapi <41652571+YarinQuapi@users.noreply.github.com>
2021-12-20 14:18:03 +0200

    Moved to more updated Bungee methods

--- src/main/java/me/yarinlevi/qpunishments/support/bungee/listeners/PlayerSwitchServerListener.java
+++ src/main/java/me/yarinlevi/qpunishments/support/bungee/listeners/PlayerSwitchServerListener.java
@@ -14,28 +14,39 @@ import java.util.NoSuchElementException;
 
 public class PlayerSwitchServerListener implements Listener {
     @EventHandler
-    public void onSwitchServer(ServerSwitchEvent event) throws SQLException {
-        if (event.getPlayer().isConnected()) {
-            String sql = String.format("SELECT * FROM punishments WHERE `punished_uuid`=\"%s\" AND `punishment_type`=\"ban\" AND `expire_date` > \"%s\" OR `punished_uuid`=\"%s\" AND `expire_date`=0 AND `punishment_type`=\"ban\"",
-                    event.getPlayer().getUniqueId().toString(), System.currentTimeMillis(), event.getPlayer().getUniqueId().toString());
+    public void onSwitchServer(ServerSwitchEvent event) {
+        QBungeePunishments.getInstance().getProxy().getScheduler().runAsync(QBungeePunishments.getInstance(), new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (event.getPlayer().isConnected()) {
+                        String sql = String.format("SELECT * FROM punishments WHERE `punished_uuid`=\"%s\" AND `punishment_type`=\"ban\" AND `expire_date` > \"%s\" OR `punished_uuid`=\"%s\" AND `expire_date`=0 AND `punishment_type`=\"ban\"",
+                                event.getPlayer().getUniqueId().toString(), System.currentTimeMillis(), event.getPlayer().getUniqueId().toString());
 
-            ResultSet rs = QBungeePunishments.getInstance().getMysql().get(sql);
+                        ResultSet rs = QBungeePunishments.getInstance().getMysql().get(sql);
 
-            try {
-                if (rs != null && rs.next() && !rs.getBoolean("bypass_expire_date") && rs.getString("server").equals(event.getPlayer().getServer().getInfo().getName())) {
-                    long timestamp = rs.getLong("expire_date");
+                        try {
+                            if (rs != null && rs.next() && !rs.getBoolean("bypass_expire_date") && rs.getString("server").equals(event.getPlayer().getServer().getInfo().getName())) {
+                                long timestamp = rs.getLong("expire_date");
 
-                    String formattedDate;
-                    if (timestamp != 0) {
-                        formattedDate = new SimpleDateFormat(MessagesUtils.getRawString("date_format")).format(new Date(timestamp));
-                    } else formattedDate = "Never";
+                                String formattedDate;
+                                if (timestamp != 0) {
+                                    formattedDate = new SimpleDateFormat(MessagesUtils.getRawString("date_format")).format(new Date(timestamp));
+                                } else formattedDate = "Never";
 
-                    //String formattedDate = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss").format((rs.getTimestamp("timestamp")));
-                    String reason = rs.getString("reason");
+                                //String formattedDate = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss").format((rs.getTimestamp("timestamp")));
+                                String reason = rs.getString("reason");
 
-                    event.getPlayer().sendMessage(MessagesUtils.getMessage("you_are_banned_chat", reason, formattedDate));
+                                event.getPlayer().sendMessage(MessagesUtils.getMessage("you_are_banned_chat", reason, formattedDate));
+                            }
+                        } catch (NoSuchElementException ignored) {
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    QBungeePunishments.getInstance().getLogger().severe("Hey! an SQL error was detected! please check your configuration files");
                 }
-            } catch (NoSuchElementException ignored) { }
-        }
+            }
+        });
     }
 }
